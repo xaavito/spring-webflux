6bf178b83929366c4a3cfb5579bdc64912dca02b
Javier Gonzalez <javiermartingonzalez@gmail.com>
2019-06-11 20:07:35 -0300

    Prueba de nuevo repo

--- src/main/java/com/prueba/spring_webflux/App.java
+++ src/main/java/com/prueba/spring_webflux/App.java
@@ -3,6 +3,11 @@ package com.prueba.spring_webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Prueba de spring webflux
+ * @author Javier Gonzalez
+ *
+ */
 @SpringBootApplication
 public class App {
 
